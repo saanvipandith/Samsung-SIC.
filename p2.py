21274499bae13baa46054544e5f0f4499682de18
ABC <saanvipandith.ec25@bmsce.ac.in>
2026-05-29 16:59:18 +0530

    Create p2.py

---
+++ p2.py
@@ -0,0 +1,19 @@
+import numpy as np
+# arange()
+
+array1 = np.arange(10)
+array2 = np.arange(10, 20)
+array3 = np.arange(10, 30, 3)
+array4 = np.ones(10)
+array5 = np.ones((2, 8))
+array6 = np.ones((3, 5))
+
+print(type(array1))
+print(array1)
+print(array2)
+print(array3)
+
+print(type(array1))
+print(array4)
+print(array5)
+print(array6)
