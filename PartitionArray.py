5f62faa320df4fe55e943dfd5641dc5460e556f0
ABC <saanvipandith.ec25@bmsce.ac.in>
2026-05-29 17:04:59 +0530

    Create PartitionArray.py

---
+++ PartitionArray.py
@@ -0,0 +1,11 @@
+def partition_array(numbers, low, high):
+    pivot = numbers[high]
+    i = low   
+    j = low 
+
+    for i in range(low, high):
+        if numbers[i] < pivot:
+            numbers[i], numbers[j] = numbers[j], numbers[i]
+            j += 1
+    numbers[high], numbers[j] = numbers[j], numbers[high]
+    return j
