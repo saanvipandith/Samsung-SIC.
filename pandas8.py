a8b28eacfaf5b6625de19446d3dee161df93b220
ABC <saanvipandith.ec25@bmsce.ac.in>
2026-05-29 16:50:39 +0530

    Create pandas8.py

---
+++ pandas8.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+data = {
+    'Name': ['nithin', 'nithya', 'nikhil', 'nishanth', 'nihal'],
+    'Subject': ['be', 'msc', 'bca', 'mtech', 'bsc'],
+    'Marks': [85.5, 80.5, 95.5, 75.5, 65.5]
+}
+
+df = pd.DataFrame(data)
+
+# Sort Data by Marks
+sorted_df = df.sort_values(by='Marks', ascending=False)
+print(sorted_df)
